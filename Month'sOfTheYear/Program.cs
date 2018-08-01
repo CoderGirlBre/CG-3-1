508060fbd793ef754237beaba0430dede29877dd
CoderGirlBre <41300229+CoderGirlBre@users.noreply.github.com>
2018-07-31 21:57:35 -0500

    Add files via upload

--- Month'sOfTheYear/Program.cs
+++ Month'sOfTheYear/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
-namespace Month_sOfTheYear
+namespace MonthsOfTheYear
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Month's of The Year");
+            Console.Write("Enter a number for the corresponding month: ");
+
+            //This makes the cosole read the number the user entered and assigns it to a variable called userValue.
+            string userValue = Console.ReadLine();
+
+            //Because this variable is dependent on IF statements, it is assigned to quotes. 
+            string message = "";
+
+            //There can only be one If statement. In order to say "or if," "else if" is used.
+            if (userValue == "1")
+                message = "January";
+            else if (userValue == "2")
+                message = "February";
+            else if (userValue == "3")
+                message = "March";
+            else if (userValue == "4")
+                message = "April";
+            else if (userValue == "5")
+                message = "May";
+            else if (userValue == "6")
+                message = "June";
+            else if (userValue == "7")
+                message = "July";
+            else if (userValue == "8")
+                message = "August";
+            else if (userValue == "9")
+                message = "September";
+            else if (userValue == "10")
+                message = "October";
+            else if (userValue == "11")
+                message = "November";
+            else if (userValue == "12")
+                message = "December";
+
+            //There can only be one Else. This is like saying "for anything else that I didn't include in the above statements, do this."
+            else
+                message = "Sorry, you've entered an invalid answer.";
+
+            //This prints the message to the console.
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+
         }
     }
 }
